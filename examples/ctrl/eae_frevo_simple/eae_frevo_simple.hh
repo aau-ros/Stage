6b55b5d8f409f9b796d7e35badbc6f3842e674eb
Micha Rappaport <micha.rappaport@aau.at>
2017-04-19 13:02:50 +0200

    added new controller stub for interfacing with frevo

---
+++ examples/ctrl/eae_frevo_simple/eae_frevo_simple.hh
@@ -0,0 +1,162 @@
+#ifndef EAE_FREVO_SIMPLE_H
+#define EAE_FREVO_SIMPLE_H
+
+#include "stage.hh"
+#include "worldfile.hh"
+
+using namespace Stg;
+using namespace std;
+
+/**
+ * @brief Namespace for energy aware exploration (eae) with frevo.
+ */
+namespace eae_frevo_simple
+{
+    /**
+     * Display additional debugging output for the robots listed in this array.
+     * Only debug output of the robots with the given ID's will be output.
+     * Robots can have following ID's: 1, 6, 11, 16, 21, 26, 31, 36, ...
+     */
+    //const int DEBUG_ROBOTS[] = {1, 6, 11, 16, 21, 26, 31, 36}; // debug all
+    const int DEBUG_ROBOTS[] = {}; // debug none
+
+    /**
+     * Distance that the robot can be away from goal.
+     */
+    const double GOAL_TOLERANCE = 1;
+
+    /**
+     * Math constant π.
+     */
+    const double PI = 3.14159265358979323846264338327950288419716939937510;
+
+    /**
+     * Value for an invalid bid.
+     */
+    const double BID_INV = 0;
+
+    /**
+     * Value for maximum bid, used for the greedy strategy.
+     */
+    const double BID_MAX = 1000000;
+
+    /**
+     * Field of view of laser scanner in radian.
+     */
+    const double LASER_FOV = 3.0/2*PI;
+
+    /**
+     * Range of laser scanner in grid cells.
+     */
+    const int LASER_RANGE = 10;
+
+    /**
+     * Map grid cell values.
+     */
+    typedef enum{
+        CELL_FREE = 0,
+        CELL_UNKNOWN,
+        CELL_OCCUPIED
+    } grid_cell_v;
+
+    /**
+     * Map grid cell coordinate type.
+     */
+    typedef struct{
+        int x;
+        int y;
+    }grid_cell_t;
+
+    /**
+     * The current state of a robot.
+     */
+    typedef enum{
+        STATE_UNDEFINED_ROBOT = 0,
+        STATE_INIT,
+        STATE_IDLE,
+        STATE_EXPLORE,
+        STATE_CHARGE_QUEUE,
+        STATE_PRECHARGE,
+        STATE_GOING_CHARGING,
+        STATE_CHARGE,
+        STATE_DEAD,
+        STATE_FINISHED
+    } robot_state_t;
+
+    /**
+     * Strings describing the robot state.
+     * Make sure they match the robot_state_t enum!
+     */
+    const string STATE_STRING[] = {"undefined", "init", "idle", "explore", "charge queue", "precharge", "going charging", "charge", "dead", "finished"};
+
+    /**
+     * Wifi message types.
+     */
+    typedef enum{
+        MSG_UNDEFINED = 0,
+        MSG_ROBOT,
+        MSG_DS,
+        MSG_FRONTIER_AUCTION,
+        MSG_DS_AUCTION,
+        MSG_DS_QUEUE,
+        MSG_MAP
+    } msg_type_t;
+
+    /**
+     * Forward class declarations.
+     */
+    class Coordination;
+    class Ds;
+    class Edge;
+    class Node;
+    class Graph;
+    class GraphVis;
+    class GridMap;
+    class LogOutput;
+    class Robot;
+    class WifiMessage;
+    class WifiMessageRobot;
+    class WifiMessageDs;
+    class WifiMessageFrontierAuction;
+    class WifiMessageDsAuction;
+    class WifiMessageDsQueue;
+    class WifiMessageMap;
+
+    /**
+     * Remove the path from a file name.
+     *
+     * @param string file: The complete file name including path.
+     *
+     * @return const char*: The file name without path.
+     */
+    inline const char* StripPath(string file)
+    {
+        return file.substr(file.find_last_of("/")+1).c_str();
+    }
+
+    /**
+     * Check if a value exists in an array.
+     * The type of the array values is the template parameter T.
+     *
+     * @param T val: The value to check.
+     * @param T arr[]: The array too look in.
+     * @param int size: The size of the array (number of elements).
+     *
+     * @return bool: True if the value exists in the array, false otherwise.
+     */
+    template<typename T>
+    inline const bool InArray(T val, const T arr[], int size)
+    {
+        // loop through array and look vor val
+        for(int i=0; i<size; ++i){
+            // value found, return true
+            if(arr[i] == val)
+                return true;
+        }
+
+        // value not found
+        return false;
+    }
+}
+
+#endif
